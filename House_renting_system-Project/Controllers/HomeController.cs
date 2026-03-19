b1166ea04062778992a58922ad90e7ea489346d3
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2026-03-19 11:44:29 +0200

    new

--- House_renting_system-Project/Controllers/HomeController.cs
+++ House_renting_system-Project/Controllers/HomeController.cs
@@ -11,11 +11,6 @@ namespace House_renting_system_Project.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
