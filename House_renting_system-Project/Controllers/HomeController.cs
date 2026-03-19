f132ec96c95ad1388666be1a224dd0e0283935a3
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2026-03-19 13:03:10 +0200

    new

--- House_renting_system-Project/Controllers/HomeController.cs
+++ House_renting_system-Project/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace House_renting_system_Project.Controllers
 {
     public class HomeController : Controller
     {
+        
         public IActionResult Index()
         {
             return View();
