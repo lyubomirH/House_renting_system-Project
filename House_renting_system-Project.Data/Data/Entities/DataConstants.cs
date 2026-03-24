798035b4324ea192d71bdc87dda1948ae2e2158c
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2026-03-24 10:04:06 +0200

    new

---
+++ House_renting_system-Project.Data/Data/Entities/DataConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace House_renting_system_Project.Data.Data.Entities
+{
+    public class DataConstants
+    {
+        public class House
+        {
+        public const int TitleMaxLength = 50;
+        public const int AddressMaxLength = 150;
+        public const int DescriptionMaxLength = 500;
+        }
+    }
+}
---
+++ House_renting_system-Project.Data/Data/Entities/House.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using static House_renting_system_Project.Data.Data.Entities.DataConstants
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Cache;
+using System.Reflection.Metadata.Ecma335;
+using System.Reflection.PortableExecutable;
+using System.Security.Principal;
+using System.Text;
+
+namespace House_renting_system_Project.Data.Data.Entities
+{
+    public class House
+    {
+        [Key]
+        public int Id { get; init; }
+
+        [MaxLength(50)]
+        [MinLength(10)]
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [MaxLength(150)]
+        [MinLength(30)]
+        [Required]
+        public string Address { get; set; } = null!;
+
+        [MaxLength(500)]
+        [MinLength(50)]
+        [Required]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [MaxLength(2000)]
+        [Required]
+        public decimal PricePerMonth { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; init; } = null!;
+
+        public int AgentId { get; set; }
+        public Agent Agent { get; set; }
+        public string? RenterId { get; set; }
+        public IdentityUser? Renter { get; set; }
+
+
+    }
+}
