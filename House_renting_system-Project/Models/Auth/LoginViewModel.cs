4a9813518d4e54ebdb2db1e045c648fb97b85736
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2026-03-26 11:29:38 +0200

    new

---
+++ House_renting_system-Project/Models/Auth/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace House_renting_system_Project.Models.Auth
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
