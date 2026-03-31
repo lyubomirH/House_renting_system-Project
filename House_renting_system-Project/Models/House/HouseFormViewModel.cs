532f7546080f1e0d9ea3b8d7f21deb832e03be8a
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2026-03-31 20:39:30 +0300

    Homework (v2) fix logic in HouseController cleand the code from moset coments NOTE: Kept some of the code for HW(v1)

--- House_renting_system-Project/Models/House/HouseFormViewModel.cs
+++ House_renting_system-Project/Models/House/HouseFormViewModel.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.ComponentModel.DataAnnotations;
 
-namespace House_renting_system_Project.Models
+namespace House_renting_system_Project.Models.House
 {
     public class HouseFormViewModel
     {
